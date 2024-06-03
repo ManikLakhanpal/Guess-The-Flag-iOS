eb6397f9a107a7f4ee71debb823a6263d1cc2708
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-03 13:26:31 +0530

    Version 1.0 created

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.indigo, .indigo, .indigo, .blue ,.blue, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                Spacer()
+                Spacer()
+                Spacer()
+                VStack {
+                    Text("Tap the flag of")
+                        .font(.title)
+                        .foregroundStyle(.yellow)
+                        .fontDesign(.rounded)
+                        .fontWeight(.black)
+                    Text(countries[correctAnswer])
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                        .fontWeight(.regular)
+                }
+                VStack(spacing: 20) {
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .cornerRadius(15)
+                                .shadow(radius: 25)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 30)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Text("Score : \(score)")
+                    .foregroundStyle(.white)
+                    .fontWeight(.regular)
+                    .font(.title3)
+                Spacer()
+                Spacer()
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your Score is : \(score)")
+        }
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,69 @@
+# WeSplit iOS Application 📱💸
+
+WeSplit is a simple iOS application built using SwiftUI that helps you split bills among friends or colleagues.
+
+## Table of Contents
+
+- [About](#about)
+- [Video Demo](#video-demo)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Contact Me](#contact-me)
+- [License](#license)
+
+## About ℹ️
+
+WeSplit is a user-friendly iOS application designed to simplify the process of splitting bills. Whether you're dining out with friends or organizing a group event, WeSplit allows you to effortlessly divide expenses among participants.
+
+## Video Demo 🎥
+
+[![Watch the video](https://i.vimeocdn.com/video/1855462231-13422a125dc3ceaa06458b24de13fd3c4afaeff68b647422a90a3e3dca6df808-d?mw=1920&mh=1080&q=70)](https://vimeo.com/953042815?share=copy#t=0)
+
+## Features ✨
+
+- Easily enter the total bill amount.
+- Select the number of people sharing the bill.
+- Choose a tip percentage or opt not to tip.
+- Instantly calculate the amount owed by each person.
+- Intuitive and user-friendly interface.
+
+## Installation 🛠️
+
+To use WeSplit on your iOS device, follow these simple steps:
+
+1. Clone or download the project repository.
+2. Open the project in Xcode.
+3. Build and run the application on your iOS device or simulator.
+
+## Usage 🚀
+
+Using WeSplit is straightforward:
+
+1. Launch the WeSplit application on your iOS device.
+2. Enter the total bill amount.
+3. Select the number of people sharing the bill using the provided picker.
+4. Choose a tip percentage from the segmented control or leave it at 0% if no tip is desired.
+5. View the calculated amount owed by each person in the "Final" section.
+
+## Contributing 🤝
+
+WeSplit is an open-source project, and contributions are welcome! If you'd like to contribute to the project, follow these steps:
+
+1. Fork the repository and create your branch.
+2. Make your changes and ensure the code follows the project's style guidelines.
+3. Test your changes thoroughly.
+4. Submit a pull request detailing the changes you've made.
+
+If you encounter any issues or bugs while using WeSplit, please feel free to contact me. I'll ensure they are addressed promptly.
+
+## Contact Me 📧
+
+If you have any questions, feedback, or suggestions, feel free to reach out to me at [lakhanpalmanik@protonmail.com](mailto:lakhanpalmanik@protonmail.com).
+
+## License 📄
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT), which means you're free to use, modify, and distribute the code as you see fit.
+
+
